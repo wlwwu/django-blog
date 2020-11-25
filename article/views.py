db3f79f6984c2962c34641e72c055b9834df70e5
dcl8zhd <linwei.wu@cn.bosch.com>
2020-11-25 16:30:05 +0800

    page nagivator

--- article/views.py
+++ article/views.py
@@ -6,7 +6,8 @@ from .forms import ArticleColumnForm,ArticlePostForm
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
-
+from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 @login_required(login_url='/account/login/')
 @csrf_exempt
@@ -62,11 +63,8 @@ def article_post(request):
                new_article.author = request.user
                new_article.column = request.user.article_column.get(id=request.POST['column_id'])
                new_article.save()
-               print("test---test")
                return HttpResponse("1")
            except Exception as inst:
-                print("test---test-1")
-                print(inst)
                 return HttpResponse("2")
         else:
             return HttpResponse("3")
@@ -76,3 +74,57 @@ def article_post(request):
         return render(request, "article/column/article_post.html",{
             "article_post_form":article_post_form,"article_columns":article_columns
         })
+
+@login_required(login_url='/account/login') 
+def article_list(request):
+    articles_list = ArticlePost.objects.filter(author=request.user)
+    paginator = Paginator(articles_list, 2)
+    page = request.GET.get('page')
+    try:
+        current_page = paginator.page(page)
+        articles = current_page.object_list
+    except PageNotAnInteger:
+        current_page = paginator.page(1)
+        articles = current_page.object_list 
+    except EmptyPage:
+        current_page = paginator.page(paginator.num_pages)
+        articles = current_page.object_list
+    return render(request, "article/column/article_list.html",{"articles":articles, "page": current_page})
+
+@login_required(login_url='/acccount/login')
+def article_detail(request,id,slug):
+    article = get_object_or_404(ArticlePost, id=id, slug=slug)
+    return render(request, "article/column/article_detail.html",{"article":article})
+
+@login_required(login_url='/account/login')
+@require_POST
+@csrf_exempt
+def del_article(request):
+    article_id = request.POST['article_id']
+    try:
+        article = ArticlePost.objects.get(id=article_id)
+        article.delete()
+        return HttpResponse("1")
+    except:
+        return HttpResponse("2")
+
+@login_required(login_url='/account/login')
+@csrf_exempt
+def redit_article(request, article_id):
+    if request.method == "GET":
+        article_columns = request.user.article_column.all()
+        article = ArticlePost.objects.get(id=article_id)
+        this_article_form = ArticlePostForm(initial={"title":article.title})
+        this_article_column = article.column
+        return render(request,"article/column/redit_article.html",{"article":article,"article_columns":article_columns,
+                                        "this_article_column":this_article_column,"this_article_form":this_article_form})
+    else:
+        redit_article = ArticlePost.objects.get(id=article_id)
+        try:
+            redit_article.column = request.user.article_column.get(id=request.POST['column_id'])
+            redit_article.title = request.POST['title']
+            redit_article.body = request.POST['body']
+            redit_article.save()
+            return HttpResponse("1")
+        except:
+            return HttpResponse("2")
